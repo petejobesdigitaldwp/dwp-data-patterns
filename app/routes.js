7614f82805f0371596aea10d728358171b706b6b
Pete Jobes <petejobes.digitaldwp@gmail.com>
2018-10-05 17:08:45 +0100

    started using routes to branch through

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,46 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-
-
 module.exports = router
+
+// Branching
+router.post('/version-0-7/where-2', function (req, res) { 
+    let whereloop = req.session.data['add-more-areas']  
+    if (whereloop === 'no') {
+      res.redirect('/version-0-7/what')
+    } else {
+      res.redirect('/version-0-7/where-2-loop')
+    }
+  })
+
+router.post('/version-0-7/where-2-loop', function (req, res) { 
+    let whereloop = req.session.data['add-more-areas']  
+    if (whereloop === 'no') {
+      res.redirect('/version-0-7/what')
+    } else {
+      res.redirect('/version-0-7/where-2-loop')
+    }
+  })
+
+  router.post('/version-0-7/what', function (req, res) { 
+    let whatloop = req.session.data['add-more-datatypes']  
+    if (whatloop === 'yes') {
+      res.redirect('/version-0-7/what-loop')
+    } else {
+        let when = req.session.data['when-choice']  
+        if (when === 'time-period') {
+          res.redirect('/version-0-7/churchill-main')
+        } else {
+          res.redirect('/version-0-7/churchill-main-snapshot')
+        }
+    }
+  })
+
+router.post('/version-0-7/what-loop', function (req, res) { 
+    let whatloop = req.session.data['add-more-datatypes']  
+    if (whatloop === 'yes') {
+      res.redirect('/version-0-7/what-loop')
+    } else {
+      res.redirect('/version-0-7/churchill-main')
+    }
+  })
