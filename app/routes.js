f5db7e5faef21a118834395f9d4a6cec771d80a4
Pete Jobes <petejobes.digitaldwp@gmail.com>
2018-10-10 11:04:01 +0100

    User testing version - 10/10/18

--- app/routes.js
+++ app/routes.js
@@ -46,3 +46,46 @@ router.post('/version-0-7/what-loop', function (req, res) {
       res.redirect('/version-0-7/churchill-main')
     }
   })
+
+
+  // Branching TEST VERSION
+router.post('/test-version/where-2', function (req, res) { 
+  let whereloop = req.session.data['add-more-areas']  
+  if (whereloop === 'no') {
+    res.redirect('/test-version/what')
+  } else {
+    res.redirect('/test-version/where-2-loop')
+  }
+})
+
+router.post('/test-version/where-2-loop', function (req, res) { 
+  let whereloop = req.session.data['add-more-areas']  
+  if (whereloop === 'no') {
+    res.redirect('/test-version/what')
+  } else {
+    res.redirect('/test-version/where-2-loop')
+  }
+})
+
+router.post('/test-version/what', function (req, res) { 
+  let whatloop = req.session.data['add-more-datatypes']  
+  if (whatloop === 'yes') {
+    res.redirect('/test-version/what-loop')
+  } else {
+      let when = req.session.data['when-choice']  
+      if (when === 'time-period') {
+        res.redirect('/test-version/churchill-main')
+      } else {
+        res.redirect('/test-version/churchill-main-snapshot')
+      }
+  }
+})
+
+router.post('/test-version/what-loop', function (req, res) { 
+  let whatloop = req.session.data['add-more-datatypes']  
+  if (whatloop === 'yes') {
+    res.redirect('/test-version/what-loop')
+  } else {
+    res.redirect('/test-version/churchill-main')
+  }
+})
